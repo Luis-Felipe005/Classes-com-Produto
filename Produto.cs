ca3102fac00663dc8141861953c4fc8256778cd8
Luis-Felipe005 <135838158+Luis-Felipe005@users.noreply.github.com>
2023-07-25 17:20:15 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseDeProduto
+{
+    internal class Produto
+    {
+
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double preco;
+        public bool status;
+
+        public void ProdutosNoConsole()
+        {
+            Console.WriteLine($"Seu produto mencionado foi {nome} \n Descrição: {descricao} \n Quantidade: {quantidade} unidades  \n Preço: R$ {preco} reais  \n Estoque:{status}");
+
+
+
+        } 
+
+        public void DesativarProduto()
+        {
+
+            quantidade= 0;
+            status= false;  
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+using ClasseDeProduto;
+
+Console.WriteLine("Olá, seja bem-vindo a loja Cunha!");
+
+Console.WriteLine("\n\n");
+
+
+Produto camisa_Nike= new Produto();
+
+
+
+camisa_Nike.nome = "Camisa Nike";
+camisa_Nike.quantidade = 300;
+camisa_Nike.preco = 70.00;
+camisa_Nike.descricao = "Camisa Nike Original - 100 algodão. Possui um ótimo custo benefício";
+camisa_Nike.status = true;
+
+camisa_Nike.ProdutosNoConsole();
+
+Console.WriteLine("\n\n");
+
+
+Produto tenis_Adidas = new Produto();
+
+
+
+tenis_Adidas.nome = "Tênis Adidas";
+tenis_Adidas.quantidade = 150;
+tenis_Adidas.preco = 300.00;
+tenis_Adidas.descricao = "Tênis Adidas original - Excelente conforto e elásticidade nos pés!";
+tenis_Adidas.status = true;
+
+tenis_Adidas.ProdutosNoConsole();
+
+Console.WriteLine("\n\n");
+
+
+
+Produto chinelo_Lacoste= new Produto(); 
+
+chinelo_Lacoste.nome = "Chinelo Lacoste";
+chinelo_Lacoste.quantidade = 100;
+chinelo_Lacoste.preco = 200.00;
+chinelo_Lacoste.descricao = "Chinelo lacoste Original - Excelente conforto e elásticidade nos pés!";
+chinelo_Lacoste.status = false;
+
+chinelo_Lacoste.DesativarProduto();
+chinelo_Lacoste.ProdutosNoConsole();
+
+Console.ReadKey();
+
+
+
+
+
+
+
